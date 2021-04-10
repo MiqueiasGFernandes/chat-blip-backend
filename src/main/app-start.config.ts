ddf7c58acfed4b8bbcb2631d240c058b85142205
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-04-10 13:08:59 -0300

    feat: add load environment variables to app-start.config

--- src/main/app-start.config.ts
+++ src/main/app-start.config.ts
@@ -1,23 +1,42 @@
 import cors from 'cors';
-import express from 'express';
+import express, { Express } from 'express';
+import dotenv from 'dotenv';
 
 class AppStart {
-  static expressInstance = express();
+  private static expressInstance: Express;
 
-  static disableHeaders() {
+  private static loadEnvinronmentVariables(): void {
+    dotenv.config({
+      path: `${__dirname}/../.env`,
+    });
+  }
+
+  private static initializeExpressInstance(): void {
+    AppStart.expressInstance = express();
+  }
+
+  private static disableHeaders(): void {
     AppStart.expressInstance.disabled('x-powered-by');
   }
 
-  static enableMiddlewares() {
+  private static enableMiddlewares(): void {
     AppStart.expressInstance.use(cors());
     AppStart.expressInstance.use(express.json());
   }
 
-  static listenServer() {
+  private static listenServer(): void {
     AppStart.expressInstance.listen(3000);
   }
 
-  static main() {
+  /**
+   * @access public
+   * @description This method is application entrypoit,
+   * avoid change order of internal methods call
+   * @returns void
+   */
+  public static main(): void {
+    AppStart.loadEnvinronmentVariables();
+    AppStart.initializeExpressInstance();
     AppStart.disableHeaders();
     AppStart.enableMiddlewares();
     AppStart.listenServer();
