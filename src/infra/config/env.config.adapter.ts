4679852416409c67944475a1348a8e646e069de5
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-04-10 20:11:25 -0300

    fix: ensure environment variable errors is trowning correctly

--- src/infra/config/env.config.adapter.ts
+++ src/infra/config/env.config.adapter.ts
@@ -1,30 +1,35 @@
-import { singleton } from "tsyringe";
+import { singleton } from 'tsyringe';
 import dotenv from 'dotenv';
-import IConfigPort from "./config.port";
-import KeyNotFound from "./key-not-found.error";
+import IConfigPort from './config.port';
+import KeyNotFound from './key-not-found.error';
 
 @singleton()
 class EnvConfigAdapter implements IConfigPort {
   constructor() {
+    const path = `${__dirname}/../../../.env`;
     dotenv.config({
-      path: `${__dirname}/../../.env`,
+      path,
     });
   }
+
   getString(key: string): string {
-    if(!process.env[key] || !!process.env[key]) throw new KeyNotFound(key);
+    if (!process.env[key]) throw new KeyNotFound(key);
     return String(process.env[key]);
   }
 
   getBoolean(key: string): boolean {
-    if(!process.env[key] || !!process.env[key]) throw new KeyNotFound(key);
+    if (process.env[key] === 'true' && process.env[key] === 'false') throw new KeyNotFound(key);
     return Boolean(process.env[key]);
   }
+
   getList(key: string): unknown[] {
-    if(!process.env[key] || !!process.env[key]) throw new KeyNotFound(key);
+    if (!process.env[key]) throw new KeyNotFound(key);
     return process.env[key]?.split(',')!;
   }
+
   getNumber(key: string): number {
-    if(!process.env[key] || !!process.env[key]) throw new KeyNotFound(key);
+    console.log(Number(process.env[key]));
+    if (!process.env[key]) throw new KeyNotFound(key);
     return Number(process.env[key]);
   }
 }
