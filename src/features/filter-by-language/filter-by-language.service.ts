f2017def9438fbe5a7abc10e614b6dcab639c6a3
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-04-11 01:43:10 -0300

    chore: add I prefix to filter by language interface

--- src/features/filter-by-language/filter-by-language.service.ts
+++ src/features/filter-by-language/filter-by-language.service.ts
@@ -1,8 +1,8 @@
 import RepositoryModel from '../_common/models/repository.model';
-import FilterByLanguage from './filter-by-language.use-case';
+import IFilterByLanguage from './filter-by-language.use-case';
 import NotFoundError from './not-found.error';
 
-class FilterByLanguageRepository implements FilterByLanguage {
+class FilterByLanguageRepository implements IFilterByLanguage {
   filter(repositoriesList: RepositoryModel[], language: string): RepositoryModel[] {
     const filteredRepositories = repositoriesList
       .filter((repository) => repository.language === language);
--- src/features/filter-by-language/filter-by-language.use-case.ts
+++ src/features/filter-by-language/filter-by-language.use-case.ts
@@ -1,7 +1,7 @@
 import RepositoryModel from '../_common/models/repository.model';
 
-interface FilterByLanguage {
+interface IFilterByLanguage {
   filter(repositoriesList: RepositoryModel[], language: string): RepositoryModel[]
 }
 
-export default FilterByLanguage;
+export default IFilterByLanguage;
