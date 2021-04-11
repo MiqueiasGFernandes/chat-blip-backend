c3f0493ff99b3a6d413a55ec83e57dbb38272cd8
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-04-11 15:27:28 -0300

    feat: add router registration

--- src/infra/di/inject-all.ts
+++ src/infra/di/inject-all.ts
@@ -1,12 +1,13 @@
+import 'reflect-metadata';
 import InfraProvider from '../infra.provider';
 import MainProvider from '../../main/main.provider';
-import ListRepositories from '../../features/list-respositories/list-repositories.provider';
+import RepositoriesProvider from '../../features/repositories/repositories.provider';
 
 class InjectAll {
   public static execute() {
     MainProvider.injectDependencies();
     InfraProvider.injectDependencies();
-    ListRepositories.injectDependencies();
+    RepositoriesProvider.injectDependencies();
   }
 }
 
--- src/main/routes-register.config.ts
+++ src/main/routes-register.config.ts
@@ -1,5 +1,18 @@
-class RoutesRegister {
+import { inject, injectable } from 'tsyringe';
+import IRouterRegister from './router-register.interface';
+import IServer from './server.insterface';
 
+@injectable()
+class RoutesRegister implements IRouterRegister {
+  private readonly router: IServer;
+
+  constructor(@inject('IServer') server: IServer) {
+    this.router = server;
+  }
+
+  public register(): void {
+    this.router.get('/repositories', 'RepositoryController');
+  }
 }
 
 export default RoutesRegister;
