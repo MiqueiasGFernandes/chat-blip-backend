1efe49aade6cfaa46c59362de3c81b67d794c9de
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-04-12 15:34:02 -0300

    ci: remove basic service from coverage

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/**.service.ts',
     '!<rootDir>/src/**/repository.service.ts',
+    '!<rootDir>/src/**/get-organization-avatar.service.ts',
   ],
 };
