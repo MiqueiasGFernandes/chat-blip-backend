2d9db205b7bab375226893f61d79e46deb76e14d
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-04-11 21:52:39 -0300

    feat: add test coverage and test watch scripts

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/**.service.ts',
+    '!<rootDir>/src/**/repository.service.ts',
+  ],
 };
