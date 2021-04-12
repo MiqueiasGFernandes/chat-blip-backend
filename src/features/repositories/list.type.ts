e605a1b03ede84e5d80ab1fca1f0881c241a7075
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-04-12 13:39:30 -0300

    refactor: create type "List" to return avatar and repositories list

---
+++ src/features/repositories/list.type.ts
@@ -0,0 +1,8 @@
+import RepositoryModel from '../_common/models/repository.model';
+
+type List = {
+  avatar: string,
+  fiveFirstRepositories: RepositoryModel[]
+}
+
+export default List;
--- src/features/repositories/repository.service.ts
+++ src/features/repositories/repository.service.ts
@@ -1,8 +1,9 @@
 import { inject, injectable } from 'tsyringe';
-import RepositoryModel from '../_common/models/repository.model';
 import IFilterByLanguage from './filter-by-language/filter-by-language.use-case';
 import IGetFiveFirstRepositories from './get-five-first-repositories/get-five-first-repositories.use-case';
+import IGetOrganizationAvatarUseCase from './get-organization-avatar/get-organization-avatar.use-case';
 import IListRepositoryUseCase from './list-respositories/list-repository.use-case';
+import List from './list.type';
 
 @injectable()
 class RepositoryService {
@@ -12,23 +13,28 @@ class RepositoryService {
 
   private readonly getFiveFirstRepositories: IGetFiveFirstRepositories;
 
+  private readonly getOrganizationAvatar: IGetOrganizationAvatarUseCase;
+
   constructor(
     @inject('FilterByLanguageUseCase') filterByLanguageUseCase: IFilterByLanguage,
     @inject('ListRepositoryUseCase') listRepositories: IListRepositoryUseCase,
     @inject('GetFiveFirstRepositories') getFiveFirstRepositories: IGetFiveFirstRepositories,
+    @inject('GetOrganizationAvatarUseCase') getOrganizationAvatar: IGetOrganizationAvatarUseCase,
   ) {
     this.filterByLanguageUseCase = filterByLanguageUseCase;
     this.listRepositores = listRepositories;
     this.getFiveFirstRepositories = getFiveFirstRepositories;
+    this.getOrganizationAvatar = getOrganizationAvatar;
   }
 
-  public async list(): Promise<RepositoryModel[]> {
+  public async list(): Promise<List> {
+    const avatar = await this.getOrganizationAvatar.getAvatar();
     const repositoriesList = await this.listRepositores.listByCreatedAscOrder();
     const filteredRepositoriesByLanguage = this.filterByLanguageUseCase
       .filter(repositoriesList, 'C#');
     const fiveFirstRepositories = this
       .getFiveFirstRepositories.getFiveFirst(filteredRepositoriesByLanguage);
-    return fiveFirstRepositories;
+    return { fiveFirstRepositories, avatar };
   }
 }
 
